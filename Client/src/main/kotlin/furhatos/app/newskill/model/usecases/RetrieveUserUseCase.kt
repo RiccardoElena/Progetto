143c63fccf773c5637829477133768499a243955
inf0 <genttneg3@gmail.com>
2025-05-28 20:21:56 +0200

    aaaaa

--- Client/src/main/kotlin/furhatos/app/newskill/model/usecases/RetrieveUserUseCase.kt
+++
@@ -1,9 +0,0 @@
-
-package furhatos.app.newskill.model.usecases
-
-import furhatos.app.newskill.model.User
-import java.util.UUID
-
-class RetrieveUserUseCase {
-    operator fun invoke(userName: String): User? = User(UUID.randomUUID(), userName, "Che squadra tifi?")
-}
