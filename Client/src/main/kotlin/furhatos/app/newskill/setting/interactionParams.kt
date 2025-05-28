8d4f5e6e8449688eb912f396e54f9271a1031540
Riccardo Elena <witherheart63@gmail.com>
2025-05-28 20:04:43 +0200

    fixed README

--- Client/src/main/kotlin/furhatos/app/newskill/setting/interactionParams.kt
+++ Client/src/main/kotlin/furhatos/app/newskill/setting/interactionParams.kt
@@ -6,7 +6,7 @@ import furhatos.util.Language
 const val MAX_NUMBER_OF_USERS = 2 // Max amount of people that Furhat will recognize as users simultaneously
 const val DISTANCE_TO_ENGAGE = 1.0 // Min distance for people to be recognised as users
 
-const val DEBUG_MODE = true
-const val TEST_MODE = true
+const val DEBUG_MODE = false
+const val TEST_MODE = false
 
 val SUPPORTED_LANGUAGES = arrayOf(Language.ITALIAN, Language.ENGLISH_GB, Language.SPANISH_ES, Language.FRENCH)
